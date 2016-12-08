93133d331b7663abc458b5c930c2eb76aea30568
extra-mayo <zjz204@nyu.edu>
2016-12-08 14:09:56 -0500

    TEXT!

--- Scenery/userInfo.js
+++ Scenery/userInfo.js
@@ -10,12 +10,13 @@ function UserInfo(){
 
 
     this.thirstHolder = document.createElement("a-entity");
-    this.hungerHolder.setAttribute("id", "thirst");
-    this.hungerHolder.setAttribute("bmfont-text", "text: Thirst:");
-    this.hungerHolder.setAttribute("position", "0.4, 0.38, -0.5");
-    this.hungerHolder.setAttribute("scale", "0.2, 0.2, 0.2");
+    this.thirstHolder.setAttribute("id", "thirst");
+    this.thirstHolder.setAttribute("bmfont-text", "text: Thirst:");
+    this.thirstHolder.setAttribute("position", "0.2, 0.38, -0.5");
+    this.thirstHolder.setAttribute("scale", "0.2, 0.2, 0.2");
 
     this.camera.appendChild(this.hungerHolder);
+    this.camera.appendChild(this.thirstHolder);
 
 
 
