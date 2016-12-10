b6f2e47a5e012006af94496a851a2bc363427d0d
skyty <tianyun_zhang@outlook.com>
2016-12-09 23:11:15 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,119 +1,125 @@
-// variable to hold a reference to our A-Frame world
 var world;
-var scene;
-var bubble;
-var snow;
+var userPosition;
+var treeArray =[];
+var rockArray = [];
+var butterflyArray = [];
+var appleArray = [];
 
-function preload() {
-    sound = loadSound('images/woosh.mp3');
-}
+var hungerLevel = 0;
+var thirstLevel = 0;
 
-var singleTree;
-var tree = [];
-var interface;
-function setup() {
-    // no canvas needed
-    noCanvas();
 
-    // construct the A-Frame world
-    // this function requires a reference to the ID of the 'a-scene' tag in our HTML document
-    world = new World('VRScene');
 
-    scene = new WinterForest(world);
-    //
-    // singleTree = new deadPineTree(3,3,0.5);
-    // world.add(tree);
-    //
-    // tree = new DAE({
-    //     asset: "waterBottle",
-    //     x: 0, y: 0, z: 0,
-    //     scaleX: 3, scaleY: 3, scaleZ: 3
-    // });
-    //
-    // world.add(tree);
+var startTime;
+
+function setup(){
+    /**
+     * Created by skyzn on 11/29/2016.
+     */
+
+    createCanvas(500, 500);
+
+    textSize(20);
+    text("world", 20, 20, 20, 20);
+    //noCanvas();
+    world = new World('Spring');
+    world.setUserPosition(0, 1, 40);
+    userPosition = world.getUserPosition();
+
+    noiseDetail(24);
+
+    var ground = new Plane({
+        x:0, y:0, z:0,
+        width:200, height:200,
+        asset:'grass',
+        repeatX:70,
+        repeatY:70,
+        rotationX:-90
+    });
+    world.add(ground);
+
+    var wish = new Plane({
+        x:-500, y:1000, z:-4000,
+        height:600, width:1200,
+        asset:'wish-pic',
+        material:"fog:false"
+    });
+    //wish.tag.setAttribute("material", "fog:false");
+    world.add(wish);
 
     var userInfo = new UserInfo();
 
-    // world.add(interface);
+    // var swarm = new butterfliesSwarm(5);
 
-    // world.setUserPosition(10, 10, 10);
+    for(var i=0; i<50; i++) {
+        treeArray.push(new voxelTree(random(-50, 50), random(-50, 50)));
+    }
 
-    // for (var i = 0; i < 10; i++){
-    //     tree.push(new deadPineTree(random(10), random(10), 0.5));
-    //     world.add(tree[i].tree);
-    // }
+    for(var i=0; i<10; i++) {
+        treeArray.push(new oneTree(random(-50, 50), random(-50, 50)));
+    }
 
-    // var deadTree = new deadPineTree(0, 0);
-    // world.add(deadTree.tree);
+    for(var i=0; i<5; i++) {
+        rockArray.push(new Rock(random(-50, 50), random(-50, 50)));
+    }
 
-    // snow = new Sphere({
-    //     x: 0,
-    //     y: 0,
-    //     z: 0,
-    //     scaleX: 1,
-    //     scaleZ: 1,
-    //     scaleY: 1,
-    //     width: 1,
-    //     height: 1,
-    //     depth: 1,
-    //     opacity: 0.9,
-    //     red: 255, green: 255, blue: 255
+
+    bubble1 = new Bubble('bubble-hunger-object', 2, 10, 20, -3.3, -0.7, 0, 1, 10);
+    bubble2 = new Bubble('bubble-thirst-object', 2, 2, 35, 0.1, -0.2, 0.25, 0.05, 5);
+
+
+
+    // object = new DAE({
+    //     asset:'bubble-thirst-object',
+    //     x:2, y: 2, z:35,
+    //     scaleX:1,
+    //     scaleY:1,
+    //     scaleZ:1
     // });
-    // world.add(snow);
-    // console.log("snow tag: ", snow.tag);
+    // world.add(object);
 
-    // bubble = new ObstacleBubble("chicken", 0.8, -2.9, -0.5, 0.8, -40, 0, 0);
-    // world.add(bubble.bubble);
-    // world.add(bubble.bubbleObject);
-    // var sandwich = new DAE({
-    //     asset: "sandwich",
-    //     x: 5,
-    //     y: 0,
-    //     z: 0,
-    //
-    //     scaleX: 0.5,
-    //     scaleY: 0.5,
-    //     scaleZ: 0.5
-    // });
-    // world.add(sandwich);
+    createButterflies(10);
+    createApples();
+
+    woodland = new DAE({
+        asset:'woodland',
+        y:-0.5,
+        scaleX:2,
+        scaleZ:2
+    });
+    world.add(woodland);
+
+    startTime = millis()/1000;
 
 
 }
 
 function draw() {
-    // if (mouseIsPressed || touchIsDown) {
-    //     world.moveUserForward(0.1);
-    //     world.add(singleTree);
-    // }
+    textSize(20);
+    text("world", 20, 20, 20, 20);
+    if (mouseIsPressed || touchIsDown) {
+        world.moveUserForward(0.1);
+    }
+    userPosition = world.getUserPosition();
+    //console.log("x:",userPosition.x, "y:", userPosition.y, "z:", userPosition.z);
 
+    if(userPosition.x >= 50 || userPosition.x <= -50 || userPosition.z >= 50 || userPosition.z <= -50) {
+        world.setUserPosition(0, 1, 40);
+    }
 
-    // scene.display();
-    // tree.move();
-    // console.log(world.getUserPosition());
+    for(var i=0; i<butterflyArray.length; i++) {
+        butterflyArray[i].fly();
+    }
 
-    // for (var i = 0; i < )
-    // bubble.move();
+    for(var i=0; i<butterflyArray.length; i++) {
+        butterflyArray[i].butterflyContainer.constrainPosition(-48, 48, 0.2, 50, -48, 48);
+    }
+
+    bubble1.updateStatus();
+    bubble1.trackUser();
+    bubble2.updateStatus();
+    bubble2.trackUser();
 
 }
-//
-// function keyTyped() {
-//     if (key == 'r') {
-//         for (let i = 0; i < world.scene.childNodes.length; i++) {
-//             if (world.scene.childNodes[i] == snow.tag) {
-//                 let temp = world.scene.childNodes[i];
-//                 console.log(temp.getAttribute("visible"));
-//                 let tempie = temp.getAttribute("visible");
-//                 if (tempie) {
-//                     world.scene.childNodes[i].setAttribute("visible", "false");
-//                 }
-//                 else {
-//                     world.scene.childNodes[i].setAttribute("visible", "true");
-//
-//                 }
-//             }
-//         }
-//     }
-//
-// }
 
 
